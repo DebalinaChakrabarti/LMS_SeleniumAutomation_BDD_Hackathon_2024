74cb0e2b6026ed8da2e6988991305f90df8ea0b6
menakanirmalkumar <133275215+menakanirmalkumar@users.noreply.github.com>
2024-05-02 17:45:22 -0400

    changes done

--- src/test/java/StepDefinitions/AssignStaffSteps.java
+++ src/test/java/StepDefinitions/AssignStaffSteps.java
@@ -15,7 +15,7 @@ public class AssignStaffSteps {
 
 	private AssignStaffPage userPage = new AssignStaffPage(DriverFactory.getDriver());
 	
-	@Given("Admin is on User Page")
+	@Given("Admin is on User Page..")
 	public void admin_is_on_User_page() throws InterruptedException {
 		try {
 			Thread.sleep(2000);
@@ -26,12 +26,12 @@ public class AssignStaffSteps {
 		userPage.clickOnUserTab();
 	}
 
-	@When("Admin clicks on {string} button")
+	@When("Admin clicks on {string} button..")
 	public void admin_clicks_button(String string) {
 	    userPage.AssignStaffModule();
 	}
 
-	@Then("User should see a pop up open for assign staff with empty form along with Save and Cancel button and close icon on the top")
+	@Then("User should see a pop up open for assign staff with empty form along with Save and Cancel button and close icon on the top..")
 	public void User_should_see_a_pop_up_open_for_assign_staff_with_save_and_cancel_button_and_close_icon_on_the_top() {
 		userPage.AssignStaffFormVisibile();
 		
--- src/test/resources/Features/05_08AssignStaffUser.feature
+++ src/test/resources/Features/05_08AssignStaffUser.feature
@@ -3,9 +3,9 @@ Feature: Assign Staff
 Background: Admin is on Manage User Page after clicks User on the navigation bar
 
 Scenario: Validate Assign Staff Popup window
-    Given Admin is on User Page
-    When Admin clicks on "Assign Staff" button
-    Then User should see a pop up open for assign staff with empty form along with Save and Cancel button and close icon on the top 
+    Given Admin is on User Page..
+    When Admin clicks on "Assign Staff" button..
+    Then User should see a pop up open for assign staff with empty form along with Save and Cancel button and close icon on the top..
   
     
     
